3ba8be6e9800ceec8b10e264adb3eec73a95ad65
gkiryaziev <linko@ukr.net>
2016-06-03 23:17:10 +0300

    [UPD] update test

--- service/vcgencmd/cpu_clean_test.go
+++ service/vcgencmd/cpu_clean_test.go
@@ -6,14 +6,20 @@ func TestClean(t *testing.T) {
 
 	var tests = []struct {
 		data     string
+		garbage  []string
 		expected string
 	}{{
 		data:     "temp=34.7'C",
+		garbage:  []string{"temp=", "'C"},
 		expected: "34.7",
+	}, {
+		data:     "volt=1.2V",
+		garbage:  []string{"volt=", "V"},
+		expected: "1.2",
 	}}
 
 	for _, test := range tests {
-		result := Clean(test.data, "temp=", "'C")
+		result := Clean(test.data, test.garbage...)
 		if test.expected != result {
 			t.Error(test.expected, "!=", result)
 		}
